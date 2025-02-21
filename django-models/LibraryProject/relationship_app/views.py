4fb076894d9b61fb80fbf0afd91f0791780ca2f4
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2025-02-22 00:27:04 +0100

    User Roles

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LogoutView
@@ -47,3 +49,27 @@ class CustomLogoutView(LogoutView):
 
 def index(request):
     return render(request, "index.html")
+
+def is_admin(user):
+    return user.userprofile == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile == 'Librarian'
+
+def is_member(user):
+    return user.userprofile == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
