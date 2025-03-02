ec53ae79ce4ec6faf3cf866779470c07b1e8f3d5
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2025-03-02 07:20:55 +0000

    custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -6,7 +6,16 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ['publication_year']
     search_fields = ('title', 'author')
 
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'username', 'date_of_birth', 'is_superuser']
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
 # Register your models here.
 admin.site.register(Book, BookAdmin)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(CustomUserManager)
