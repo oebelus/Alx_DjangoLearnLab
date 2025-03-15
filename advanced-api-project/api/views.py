a052ad147afd976ab211e3939ef13e031cf4f692
oebelus <imaneelfidrissi@hotmail.com>
2025-03-15 00:55:58 +0000

    IsAuthenticatedOrReadOnly permission

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from .serializers import BookSerializer
