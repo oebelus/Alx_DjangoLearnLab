b026a2ee5025836faf1828970beeceb6c8c14511
oebelus <imaneelfidrissi@hotmail.com>
2025-03-15 00:54:11 +0000

    adding create to urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),
     path('books/<int:pk>/', DetailView.as_view(), name='book-details'),
-    path('books/add_book/', CreateView.as_view(), name='add-book'),
+    path('books/create/', CreateView.as_view(), name='add-book'),
     path('books/update/<int:pk>/', UpdateView.as_view(), name='update-book'),
     path('books/delete/<int:pk>', DeleteView.as_view(), name='delete-book'),
 ]
