2b7898664dd8995a52c6c89f25f152a4be7f2f2e
oebelus <imaneelfidrissi@hotmail.com>
2025-03-21 15:34:45 +0000

    tag

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('comment/<int:pk>/update/', edit_comment, name='edit-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
     path('search/', search, name='search'),
-    path('tags/<str:tag_name>/', posts_by_tag, name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
 
 if settings.DEBUG:
