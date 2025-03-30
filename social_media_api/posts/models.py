f0056ca71a8ce2fefad19ab81c0da66a923124a9
oebelus <imaneelfidrissi@hotmail.com>
2025-03-30 14:34:43 +0000

    Notifications and Likes

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -2,26 +2,27 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
     author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.content
-    
+
 class Like(models.Model):
     post = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
     author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.author.username} liked {self.post.title}"
